2f9c65ab23aadcea3dacf917470d3793f5f80b6b
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-11 21:59:15 +0300

    add merge sort test class

--- src/test/java/com/mergesort/MergeSortTest.java
+++ src/test/java/com/mergesort/MergeSortTest.java
@@ -2,9 +2,7 @@ package com.mergesort;
 
 import junit.framework.TestCase;
 
-/**
- * Evg. Moskalenko | http://www.evgmoskalenko.com/
- */
+
 public class MergeSortTest extends TestCase {
 
     public void testMain() throws Exception {
