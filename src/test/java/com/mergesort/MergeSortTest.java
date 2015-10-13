e23bb0bf34488143e137b47ea283ba4bb16607ea
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-13 23:22:45 +0300

    JUnit

--- src/test/java/com/mergesort/MergeSortTest.java
+++ src/test/java/com/mergesort/MergeSortTest.java
@@ -1,11 +1,13 @@
 package com.mergesort;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 
 public class MergeSortTest extends TestCase {
 
+    @Test()
     public void testMain() throws Exception {
-
+        System.out.println("====================");
     }
 }
