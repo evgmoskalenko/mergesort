9260bf3f3e38ae73090a4951f94eb6ecfd992a43
freedomserg <freedomserg97@gmail.com>
2015-10-14 20:52:03 +0300

    fix

--- src/main/java/com/mergesort/Main.java
+++ src/main/java/com/mergesort/Main.java
@@ -4,5 +4,7 @@ package com.mergesort;
 public class Main {
     public static void main(String[] args) {
         MergeSort mergeSort = new MergeSort();
+        int[] arr = {5, 7, 8, 10};
+        mergeSort.sortArray(arr);
     }
 }
