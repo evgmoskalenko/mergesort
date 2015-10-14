ca82549f511365eec3117cc0fa82057d2fffa470
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-15 00:40:01 +0300

    create: choose data, input by keyboard, read txt files

--- src/main/java/com/mergesort/InputData.java
+++ src/main/java/com/mergesort/InputData.java
@@ -1,26 +1,51 @@
 package com.mergesort;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Please choose input data: 1 - from txt files, 2 - from keyboard.
- * If you choose: '1', but the file does not exist, automatically input by keyboard
+ * If file does not exist - default input by keyboard.
  */
 
 public class InputData {
 
-    public static void main(String[] args) {
-        chooseData();
-    }
-
     public static void chooseData() {
-        System.out.println("Please choose input data: 1 - from txt files, 2 - from keyboard. \nIf you choose: '1', but the file does not exist, automatically input by keyboard");
+        System.out.println("\n Please choose input data: 1 - from txt files, 2 - from keyboard.\n If you choose: '1', and the file does not exist - default input by keyboard");
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n Choose: 1 - from txt files, 2 - from keyboard");
+        int methodDataEntry = in.nextInt();
+        if (methodDataEntry == 1) {
+            //System.out.println("\nYour choose: 1 - from txt files");
+            filesData();
+        } else {
+            //System.out.println("\nYour choose: 2 - from keyboard");
+            keyboardData();
+        }
     }
 
-//    public int[] keyboardData() {
-//
-//    }
-//
-//    public int[] filesData() {
-//
-//    }
+    public static void filesData() {
+        try (FileReader reader = new FileReader("/Users/apple/IdeaProjects/mergesort/files/inputdata.txt")) {
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
+    public static int[] keyboardData() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the length of the array:");
+        int arrayLength = in.nextInt();
+        System.out.println("Input " + arrayLength + " numbers");
+        int[] array = new int[arrayLength];
+        for (int i = 0; i < arrayLength; i++) {
+            int inputDataNumber = in.nextInt();
+            array[i] = inputDataNumber;
+        }
+        return array;
+    }
 }
