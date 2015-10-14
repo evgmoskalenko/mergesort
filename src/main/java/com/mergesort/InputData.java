277c94445970cf2851a2607e5e0479b8b74336c2
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-14 22:32:50 +0300

    add input data class with methods

---
+++ src/main/java/com/mergesort/InputData.java
@@ -0,0 +1,26 @@
+package com.mergesort;
+
+/**
+ * Please choose input data: 1 - from txt files, 2 - from keyboard.
+ * If you choose: '1', but the file does not exist, automatically input by keyboard
+ */
+
+public class InputData {
+
+    public static void main(String[] args) {
+        chooseData();
+    }
+
+    public static void chooseData() {
+        System.out.println("Please choose input data: 1 - from txt files, 2 - from keyboard. \nIf you choose: '1', but the file does not exist, automatically input by keyboard");
+    }
+
+//    public int[] keyboardData() {
+//
+//    }
+//
+//    public int[] filesData() {
+//
+//    }
+
+}
