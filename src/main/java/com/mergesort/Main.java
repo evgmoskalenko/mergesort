2729cd16c46248143cdb3bcd64d800d180242f39
Evgeniy Moskalenko <evg@evgmoskalenko.com>
2015-10-15 00:41:03 +0300

    add InputData to main class

--- src/main/java/com/mergesort/Main.java
+++ src/main/java/com/mergesort/Main.java
@@ -3,6 +3,9 @@ package com.mergesort;
 
 public class Main {
     public static void main(String[] args) {
+        InputData inputData = new InputData();
+        inputData.chooseData();
+
         MergeSort mergeSort = new MergeSort();
         int[] arr = {5, 7, 8, 10};
         mergeSort.sortArray(arr);
