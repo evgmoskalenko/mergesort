ec2b719c82b5a8d8d1319592c9bb57bf5ced33c2
freedomserg <freedomserg97@gmail.com>
2015-10-15 00:10:41 +0300

    last fix of class MergeSort

--- src/main/java/com/mergesort/Main.java
+++ src/main/java/com/mergesort/Main.java
@@ -4,7 +4,11 @@ package com.mergesort;
 public class Main {
     public static void main(String[] args) {
         MergeSort mergeSort = new MergeSort();
-        int[] arr = {5, 7, 8, 10};
+        int[] arr = {5, 17, 12, 7};
         mergeSort.sortArray(arr);
+
+        for (int i : mergeSort.getResult()) {
+            System.out.print(i + " ");
+        }
     }
 }
