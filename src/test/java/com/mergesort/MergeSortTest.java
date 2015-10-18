e9d6599d43087dda8578e80ea215a99db00149a1
freedomserg <freedomserg97@gmail.com>
2015-10-18 17:25:44 +0300

    Add test

--- src/test/java/com/mergesort/MergeSortTest.java
+++ src/test/java/com/mergesort/MergeSortTest.java
@@ -1,16 +1,24 @@
 package com.mergesort;
 
+import junit.framework.AssertionFailedError;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MergeSortTest {
 
     @Test()
     public void mergeSortTest() throws Exception {
-        for (int i = 0; i < 10; i++) {
-            int[] arr = new int[10];
+        for (int i = 0; i < 100; i++) {
+            int[] arr = new int[50];
             Random random = new Random();
             for (int j = 0; j < arr.length; j++) {
                 arr[j] = random.nextInt(200);
@@ -23,4 +31,22 @@ public class MergeSortTest {
             }
         }
     }
+
+    @Test()
+    public void mergeSortTest2() throws Exception {
+        int[] initial = {4, 7, 0, -10};
+        int[] expected = {-10, 0, 4, 7};
+        MergeSort.doSort(initial);
+        Assert.assertArrayEquals(expected, initial);
+    }
+
+    @Test()
+    public void mergeSortTest3() throws Exception {
+        int[] initial = {4, 7, 0, -10, -7};
+        int[] expected = {4, 7, 0, -10, -7};
+        MergeSort.doSort(initial);
+        Arrays.sort(expected);
+        Assert.assertArrayEquals(expected, initial);
+    }
+
 }
